c0c1ce8687c59c88eff74060607d56f8fd8abc85
Cadence Marseille <cadencemarseille@gmail.com>
2013-10-01 07:23:21 -0400

    Add an AUTHORS file

--- src/pcre.rs
+++ src/pcre.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 Cadence Marseille.
+// Copyright 2013 The rust-pcre authors.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
