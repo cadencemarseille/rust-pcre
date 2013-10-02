ac406d76b7ef4aa40ba667d5a7763b5a307717b5
Cadence Marseille <cadencemarseille@gmail.com>
2013-10-02 07:40:05 -0400

    Wrap `pcre_study()`

--- src/pcre.rs
+++ src/pcre.rs
@@ -15,6 +15,7 @@ use std::ptr;
 use std::vec;
 
 pub type options = c_int;
+pub type study_options = c_int;
 
 pub static PCRE_CASELESS: options = 0x00000001;
 pub static PCRE_MULTILINE: options = 0x00000002;
@@ -56,6 +57,11 @@ pub static PCRE_PARTIAL_HARD: options = 0x08000000;
 pub static PCRE_NOTEMPTY_ATSTART: options = 0x10000000;
 pub static PCRE_UCP: options = 0x20000000;
 
+pub static PCRE_STUDY_JIT_COMPILE: study_options = 0x0001;
+pub static PCRE_STUDY_JIT_PARTIAL_SOFT_COMPILE: study_options = 0x0002;
+pub static PCRE_STUDY_JIT_PARTIAL_HARD_COMPILE: study_options = 0x0004;
+pub static PCRE_STUDY_EXTRA_NEEDED: study_options = 0x0008;
+
 mod detail {
 
     use std::c_str::*;
@@ -84,6 +90,7 @@ mod detail {
             fn pcre_exec(code: *::detail::pcre, extra: *::detail::pcre_extra, subject: *c_char, length: c_int, startoffset: c_int, options: ::options, ovector: *mut c_int, ovecsize: c_int) -> ::detail::pcre_error;
             fn pcre_free_study(extra: *::detail::pcre_extra);
             fn pcre_fullinfo(code: *::detail::pcre, extra: *::detail::pcre_extra, what: ::detail::fullinfo_field, where: *mut c_void) -> c_int;
+            fn pcre_study(code: *::detail::pcre, options: ::study_options, errptr: *mut *c_char) -> *::detail::pcre_extra;
         }
 
     }
@@ -150,6 +157,30 @@ mod detail {
         if rc < 0 && rc != PCRE_ERROR_NULL {
             fail!("pcre_fullinfo");
         }
+    }
+
+    #[fixed_stack_segment]
+    #[inline(never)]
+    pub fn pcre_study(code: *::detail::pcre, options: ::study_options) -> *::detail::pcre_extra {
+        assert!(ptr::is_not_null(code));
+        let mut err: *c_char = ptr::null();
+        let extra = unsafe { native::pcre_study(code, options, &mut err) };
+        // "The third argument for pcre_study() is a pointer for an error message. If
+        // studying succeeds (even if no data is returned), the variable it points to is
+        // set to NULL. Otherwise it is set to point to a textual error message. This is
+        // a static string that is part of the library. You must not try to free it."
+        // http://pcre.org/pcre.txt
+        if ptr::is_not_null(err) {
+            let err_cstring = unsafe { CString::new(err, false) };
+            match err_cstring.as_str() {
+                None          => error!("pcre_study() failed"),
+                Some(err_str) => error!("pcre_study() failed: %s", err_str)
+            }
+            fail!("pcre_study");
+        }
+        assert!(ptr::is_null(err));
+
+        extra
     }
 
 } // End `mod detail`.
@@ -228,6 +259,20 @@ impl Pcre {
                 }
             }
         }
+    }
+
+    fn study(&mut self) -> bool {
+        self.study_with_options(0)
+    }
+
+    fn study_with_options(&mut self, options: study_options) -> bool {
+        // Free any current study data.
+        detail::pcre_free_study(self.extra);
+        self.extra = ptr::null();
+
+        let extra = detail::pcre_study(self.code, options);
+        self.extra = extra;
+        ptr::is_not_null(extra)
     }
 }
 
@@ -318,5 +363,18 @@ mod tests {
         assert_eq!(m1.group_len(0u), 3u);
         let m2 = re.exec(subject).unwrap();
         assert_eq!(m2.group_start(0u), 0u);
+    }
+
+    #[test]
+    fn test_study_basic() {
+        let mut re = ::Pcre::compile("abc");
+        let mut study_res = re.study();
+        assert!(study_res);
+        // Re-study the pattern two more times (to check for leaks when the test program
+        // is run through Valgrind).
+        study_res = re.study();
+        assert!(study_res);
+        study_res = re.study();
+        assert!(study_res);
     }
 }
