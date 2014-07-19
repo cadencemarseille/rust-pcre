0c24a307506a8c92d5b158f5811cbf7d7faef05e
Cadence Marseille <cadencemarseille@gmail.com>
2014-07-19 07:29:18 -0400

    Revert "Remove the unused #[crate_id] attribute"
    This reverts commit cb6312d584ae41c33a96512be619a950d0174229.

--- src/pcre/mod.rs
+++ src/pcre/mod.rs
@@ -6,6 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![crate_id = "pcre#0.1"]
 #![crate_type = "lib"]
 #![feature(phase)]
 #![feature(unsafe_destructor)]
